5cc29fd3beb98b7f47255909dc0cbac3b94074e9
epanlauw <epanlauw6@gmail.com>
2023-09-12 22:44:43 +0700

    convert sign up page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/detail.css";
 import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
+import "../styles/sign-up.css";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
