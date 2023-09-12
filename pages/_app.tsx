5517b1a40b0ebf22a6b6476d1510bbfe9917c06d
epanlauw <epanlauw6@gmail.com>
2023-09-12 19:01:40 +0700

    convert detail page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "../styles/utilities.css";
 import "../styles/homepage.css";
+import "../styles/detail.css";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
