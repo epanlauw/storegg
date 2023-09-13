306bcecf51de98326edd7212de97f959dbb7f7e6
epanlauw <epanlauw6@gmail.com>
2023-09-13 17:10:59 +0700

    refactor navbar to organism

---
+++ components/organisms/Navbar/data.ts
@@ -0,0 +1,29 @@
+const MenuItem = [
+  {
+    title: "Home",
+    active: true,
+    href: "/"
+  },
+  {
+    title: "Games",
+    active: false,
+    href: "/games"
+  },
+  {
+    title: "Reward",
+    active: false,
+    href: "/reward"
+  },
+  {
+    title: "Discover",
+    active: false,
+    href: "/discover"
+  },
+  {
+    title: "Global Rank",
+    active: false,
+    href: "/global-rank"
+  }
+];
+
+export default MenuItem;
